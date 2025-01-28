7f32b5911196ae820f2be0ee19bdb06685b13a87
RadioAddition <radioaddition@pm.me>
2025-01-28 09:36:15 -0500

    start work on using greetd

--- base/DEs/hyprland.nix
+++ base/DEs/hyprland.nix
@@ -1,4 +1,9 @@
 { lib, pkgs, config, inputs, ... }: {
+  # First a greeter, since hyprland doesn't come with one
+  services.greetd = {
+    enable = true;
+    package = pkgs.nwg-hello;
+  };
   programs.hyprland = {
     enable = true;
   };
