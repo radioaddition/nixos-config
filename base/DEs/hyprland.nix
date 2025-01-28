cffe36a9323a51e4437b6777a14029bf3ad7510a
RadioAddition <radioaddition@pm.me>
2025-01-28 09:52:20 -0500

    add greetd launch options

--- base/DEs/hyprland.nix
+++ base/DEs/hyprland.nix
@@ -3,6 +3,9 @@
   services.greetd = {
     enable = true;
     package = pkgs.nwg-hello;
+    settings.default_session.command = ''
+      ${ lib.makeBinPath [ pkgs.greetd.gtkgreet ] }/gtkgreet -l -c Hyprland
+    '';
   };
   programs.hyprland = {
     enable = true;
