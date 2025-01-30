1bcb29c714fba551bc094044072dd009073d0e7a
RadioAddition <radioaddition@pm.me>
2025-01-29 20:42:46 -0500

    fix kernel version code

--- base/security.nix
+++ base/security.nix
@@ -111,8 +111,8 @@
                 url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
                 sha256 = "0ibayrvrnw2lw7si78vdqnr20mm1d3z0g6a0ykndvgn5vdax5x9a";
           };
-          version = "6.13.0";
-          modDirVersion = "6.13.0";
+          version = "6.13";
+          modDirVersion = "6.13";
         };
       });
 
