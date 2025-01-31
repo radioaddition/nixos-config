ffc68d4b3ae5e8dfdc3e9a95f8e935294ad4ce84
RadioAddition <radioaddition@pm.me>
2025-01-31 16:47:27 -0500

    install new package

--- base/programs/flatpak.nix
+++ base/programs/flatpak.nix
@@ -16,6 +16,11 @@
       }
     ];
     packages = [
+      # Sober, for roblox
+      {
+        flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
+	sha256 = "1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
+      }
       # Cartridges, gnome-styled version of lutris
       {
         appId = "page.kramo.Cartridges";
