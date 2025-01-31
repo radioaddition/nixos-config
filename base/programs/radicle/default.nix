5fd9b85be4e0eb2cdec90d45d9954431ec1a378c
RadioAddition <radioaddition@pm.me>
2025-01-31 11:21:34 -0500

    add radicle and agenix

---
+++ base/programs/radicle/default.nix
@@ -0,0 +1,9 @@
+{ lib, config, pkgs, ... }: {
+  age.secrets.radicle.file = ./radicle.age;
+  services.radicle = {
+    enable = true;
+    httpd.enable = true;
+    node.openFirewall = true;
+    privateKeyFile = config.age.secrets.radicle.file;
+  };
+}
---
+++ secrets.nix
@@ -0,0 +1,6 @@
+let
+  system = "age1nru4k2005d8820gxn03cfhlqcs5xfq0ycqjwtzsnzmkd2en5jc8q6dtsfq";
+in
+{
+  "base/programs/radicle/radicle.age".publicKeys = [ system ];
+}
