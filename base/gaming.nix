8cd44d755e05f104d8db3ab9840c90957dcc8845
RadioAddition <radioaddition@pm.me>
2025-02-21 23:05:08 -0500

    remove some gaming stuff from the specialisation

--- base/gaming.nix
+++ base/gaming.nix
@@ -5,53 +5,59 @@
   ...
 }:
 {
+  ## Steam
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    localNetworkGameTransfers.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    remotePlay.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  programs.gamescope.enable = true;
+  programs.java.enable = true;
+  environment.systemPackages = with pkgs; [
+    mangohud
+    # protonplus
+    adwsteamgtk
+  ];
+
+  jovian = {
+    steam = {
+      enable = true;
+      updater.splash = "vendor";
+      user = "radioaddition";
+    };
+    devices.steamdeck.enable = false;
+    decky-loader.enable = true;
+    steamos = {
+      useSteamOSConfig = true;
+      enableZram = false;
+      enableProductSerialAccess = false;
+    };
+    # steamos.enableMesaPatches = false;
+    hardware.has.amd.gpu = true;
+  };
+
   specialisation.gaming-mode.configuration = {
     imports = [ inputs.jovian.nixosModules.jovian ];
     # Disable kernel hardening in gaming mode for performance
     boot.kernelParams = lib.mkForce [ ];
 
     # Disable gdm so that jovian autostart will work
-    # services.xserver.displayManager.gdm.enable = lib.mkForce false;
-    # OpenGL
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
-    services.xserver.videoDrivers = [ "amdgpu" ];
+    services.xserver.displayManager.gdm.enable = lib.mkForce false;
 
-    ## Steam
-    programs.steam = {
-      enable = true;
-      # extraCompatPackages = with pkgs; [ proton-ge-bin ];
-      localNetworkGameTransfers.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      remotePlay.openFirewall = true;
-      gamescopeSession.enable = true;
+    jovian.steam = {
+      autoStart = true;
+      desktopSession = "gnome";
     };
-    programs.gamescope.enable = true;
-    programs.java.enable = true;
-    environment.systemPackages = with pkgs; [
-      mangohud
-      protonplus
-      adwsteamgtk
-    ];
-    jovian = {
-      steam = {
-        enable = true;
-        # autoStart = true;
-        # desktopSession = "gnome";
-        updater.splash = "vendor";
-        user = "radioaddition";
-      };
-      devices.steamdeck.enable = false;
-      decky-loader.enable = true;
-      steamos = {
-        useSteamOSConfig = true;
-        enableZram = false;
-        enableProductSerialAccess = false;
-      };
-      # steamos.enableMesaPatches = false;
-      hardware.has.amd.gpu = true;
-    };
+
   };
 }
