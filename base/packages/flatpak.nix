23bcbce2655523616f5256f25df7ef87d31943e8
RadioAddition <radioaddition@pm.me>
2025-09-27 15:16:09 -0400

    package changes and I finally sorted my flatpaks

--- base/packages/flatpak.nix
+++ base/packages/flatpak.nix
@@ -15,242 +15,104 @@
       }
     ];
     packages = [
-      # Sober, for roblox
-      {
-        appId = "org.vinegarhq.Sober";
-        origin = "flathub";
-      }
-      # torrhunt
-      {
-        appId = "com.ktechpit.torrhunt";
-        origin = "flathub";
-      }
-      # dino
-      {
-        appId = "im.dino.Dino";
-        origin = "flathub";
-      }
-      #protonmail-bridge
-      {
-        appId = "ch.protonmail.protonmail-bridge";
-        origin = "flathub";
-      }
-      # GoodCord
-      "io.github.milkshiift.GoofCord"
-      # Warp, a wormhole client
-      {
-        appId = "app.drey.Warp";
-        origin = "flathub";
-      }
-      # Fotema, a photo viewer
-      {
-        appId = "app.fotema.Fotema";
-        origin = "flathub";
-      }
-      # SimpleX Chat
-      {
-        appId = "chat.simplex.simplex";
-        origin = "flathub";
-      }
-      {
-        appId = "org.torproject.torbrowser-launcher";
-        origin = "flathub";
-      }
-      # GPU Screen recorder
-      {
-        appId = "com.dec05eba.gpu_screen_recorder";
-        origin = "flathub";
-      }
-      # Pods, a podman manager
-      {
-        appId = "com.github.marhkb.Pods";
-        origin = "flathub";
-      }
-      # Flatseal, a flatpak permissions manager
-      {
-        appId = "com.github.tchx84.Flatseal";
-        origin = "flathub";
-      }
-      # EasyEffects
-      {
-        appId = "com.github.wwmm.easyeffects";
-        origin = "flathub";
-      }
       # Bottles, a wine prefix manager
-      {
-        appId = "com.usebottles.bottles";
-        origin = "flathub";
-      }
-      # KTorrent
-      {
-        appId = "org.kde.ktorrent";
-        origin = "flathub";
-      }
-      # Snoop, search through file contents (rather than just names)
-      {
-        appId = "de.philippun1.Snoop";
-        origin = "flathub";
-      }
-      # Metadata cleaner
-      {
-        appId = "fr.romainvigier.MetadataCleaner";
-        origin = "flathub";
-      }
-      # Ente Auth, 2FA app
-      {
-        appId = "io.ente.auth";
-        origin = "flathub";
-      }
-      # Ignition, startup process manager
-      {
-        appId = "io.github.flattool.Ignition";
-        origin = "flathub";
-      }
-      # Warehouse, flatpak manager
-      {
-        appId = "io.github.flattool.Warehouse";
-        origin = "flathub";
-      }
-      # Flatsweep, flatpak remnant cleaner
-      {
-        appId = "io.github.giantpinkrobots.flatsweep";
-        origin = "flathub";
-      }
-      # Video wallpaper
-      {
-        appId = "io.github.jeffshee.Hidamari";
-        origin = "flathub";
-      }
+      { appId = "com.usebottles.bottles"; origin = "flathub"; }
+      # Dino
+      { appId = "im.dino.Dino"; origin = "flathub"; }
       # Discord client
-      {
-        appId = "io.github.milkshiift.GoofCord";
-        origin = "flathub";
-      }
-      # LLM app
-      {
-        appId = "io.github.qwersyk.Newelle";
-        origin = "flathub";
-      }
-      # GDM Settings
-      {
-        appId = "io.github.realmazharhussain.GdmSettings";
-        origin = "flathub";
-      }
-      # WiVRn, SteamVR alternative
-      {
-        appId = "io.github.wivrn.wivrn";
-        origin = "flathub";
-      }
-      # Minecraft Bedrock Launcher
-      {
-        appId = "io.mrarm.mcpelauncher";
-        origin = "flathub";
-      }
-      # Fedora Media Writer
-      {
-        appId = "org.fedoraproject.MediaWriter";
-        origin = "flathub";
-      }
-      # GNOME Calculator
-      {
-        appId = "org.gnome.Calculator";
-        origin = "flathub";
-      }
-      # GNOME Calendar
-      {
-        appId = "org.gnome.Calendar";
-        origin = "flathub";
-      }
-      # GNOME Connection manager
-      {
-        appId = "org.gnome.Connections";
-        origin = "flathub";
-      }
-      # GNOME Contacts
-      {
-        appId = "org.gnome.Contacts";
-        origin = "flathub";
-      }
-      # Document viewer
-      {
-        appId = "org.gnome.Evince";
-        origin = "flathub";
-      }
-      # GNOME Logs
-      {
-        appId = "org.gnome.Logs";
-        origin = "flathub";
-      }
-      # Image Viewer
-      {
-        appId = "org.gnome.Loupe";
-        origin = "flathub";
-      }
-      # GNOME Screenshot
-      {
-        appId = "org.gnome.Snapshot";
-        origin = "flathub";
-      }
-      # GNOME Text Editor
-      {
-        appId = "org.gnome.TextEditor";
-        origin = "flathub";
-      }
-      # Gnome Keepass client
-      {
-        appId = "org.gnome.World.Secrets";
-        origin = "flathub";
-      }
+      { appId = "io.github.milkshiift.GoofCord"; origin = "flathub"; }
       # Disk usage analyzer
-      {
-        appId = "org.gnome.baobab";
-        origin = "flathub";
-      }
+      { appId = "org.gnome.baobab"; origin = "flathub"; }
+      # Document viewer
+      { appId = "org.gnome.Evince"; origin = "flathub"; }
+      # EasyEffects
+      { appId = "com.github.wwmm.easyeffects"; origin = "flathub"; }
+      # Ente Auth, 2FA app
+      { appId = "io.ente.auth"; origin = "flathub"; }
+      # Fedora Media Writer
+      { appId = "org.fedoraproject.MediaWriter"; origin = "flathub"; }
+      # Fightcade
+      { appId = "com.fightcade.Fightcade"; origin = "flathub"; }
+      # Flatseal, a flatpak permissions manager
+      { appId = "com.github.tchx84.Flatseal"; origin = "flathub"; }
+      # Flatsweep, flatpak remnant cleaner
+      { appId = "io.github.giantpinkrobots.flatsweep"; origin = "flathub"; }
+      # Fotema, a photo viewer
+      { appId = "app.fotema.Fotema"; origin = "flathub"; }
+      # GDM Settings
+      { appId = "io.github.realmazharhussain.GdmSettings"; origin = "flathub"; }
+      # GNOME Calculator
+      { appId = "org.gnome.Calculator"; origin = "flathub"; }
+      # GNOME Calendar
+      { appId = "org.gnome.Calendar"; origin = "flathub"; }
       # GNOME Clock
-      {
-        appId = "org.gnome.clocks";
-        origin = "flathub";
-      }
+      { appId = "org.gnome.clocks"; origin = "flathub"; }
+      # GNOME Connection manager
+      { appId = "org.gnome.Connections"; origin = "flathub"; }
+      # GNOME Contacts
+      { appId = "org.gnome.Contacts"; origin = "flathub"; }
       # GNOME Font Viewer
-      {
-        appId = "org.gnome.font-viewer";
-        origin = "flathub";
-      }
-      # Markdown text editor
-      {
-        appId = "org.gnome.gitlab.somas.Apostrophe";
-        origin = "flathub";
-      }
+      { appId = "org.gnome.font-viewer"; origin = "flathub"; }
+      # GNOME Logs
+      { appId = "org.gnome.Logs"; origin = "flathub"; }
+      # GNOME Screenshot
+      { appId = "org.gnome.Snapshot"; origin = "flathub"; }
+      # GNOME Text Editor
+      { appId = "org.gnome.TextEditor"; origin = "flathub"; }
+      # GPU Screen recorder
+      { appId = "com.dec05eba.gpu_screen_recorder"; origin = "flathub"; }
+      # Gnome Keepass client
+      { appId = "org.gnome.World.Secrets"; origin = "flathub"; }
+      # GoodCord
+      { appId = "io.github.milkshiift.GoofCord"; origin = "flathub"; }
+      # Ignition, startup process manager
+      { appId = "io.github.flattool.Ignition"; origin = "flathub"; }
+      # Image Viewer
+      { appId = "org.gnome.Loupe"; origin = "flathub"; }
+      # KTorrent
+      { appId = "org.kde.ktorrent"; origin = "flathub"; }
       # KeePassXC
-      {
-        appId = "org.keepassxc.KeePassXC";
-        origin = "flathub";
-      }
+      { appId = "org.keepassxc.KeePassXC"; origin = "flathub"; }
+      # LLM app
+      { appId = "io.github.qwersyk.Newelle"; origin = "flathub"; }
       # LocalSend
-      {
-        appId = "org.localsend.localsend_app";
-        origin = "flathub";
-      }
-      # Parabolic
-      {
-        appId = "org.nickvision.tubeconverter";
-        origin = "flathub";
-      }
-      # PrismLauncher, minecraft launcher
-      {
-        appId = "org.prismlauncher.PrismLauncher";
-        origin = "flathub";
-      }
-      # Signal
-      {
-        appId = "org.signal.Signal";
-        origin = "flathub";
-      }
+      { appId = "org.localsend.localsend_app"; origin = "flathub"; }
+      # Markdown text editor
+      { appId = "org.gnome.gitlab.somas.Apostrophe"; origin = "flathub"; }
       # Menu item editor
-      {
-        appId = "page.codeberg.libre_menu_editor.LibreMenuEditor";
-        origin = "flathub";
-      }
+      { appId = "page.codeberg.libre_menu_editor.LibreMenuEditor"; origin = "flathub"; }
+      # Metadata cleaner
+      { appId = "fr.romainvigier.MetadataCleaner"; origin = "flathub"; }
+      # Minecraft Bedrock Launcher
+      { appId = "io.mrarm.mcpelauncher"; origin = "flathub"; }
+      # Parabolic
+      { appId = "org.nickvision.tubeconverter"; origin = "flathub"; }
+      # Pods, a podman manager
+      { appId = "com.github.marhkb.Pods"; origin = "flathub"; }
+      # PrismLauncher, minecraft launcher
+      { appId = "org.prismlauncher.PrismLauncher"; origin = "flathub"; }
+      # Protonmail-bridge
+      { appId = "ch.protonmail.protonmail-bridge"; origin = "flathub"; }
+      # Signal
+      { appId = "org.signal.Signal"; origin = "flathub"; }
+      # SimpleX Chat
+      { appId = "chat.simplex.simplex"; origin = "flathub"; }
+      # Snoop, search through file contents (rather than just names)
+      { appId = "de.philippun1.Snoop"; origin = "flathub"; }
+      # Sober, for roblox
+      { appId = "org.vinegarhq.Sober"; origin = "flathub"; }
+      # Tor Browser
+      { appId = "org.torproject.torbrowser-launcher"; origin = "flathub"; }
+      # Torrhunt
+      { appId = "com.ktechpit.torrhunt"; origin = "flathub"; }
+      # Video wallpaper
+      { appId = "io.github.jeffshee.Hidamari"; origin = "flathub"; }
+      # Warehouse, flatpak manager
+      { appId = "io.github.flattool.Warehouse"; origin = "flathub"; }
+      # Warp, a wormhole client
+      { appId = "app.drey.Warp"; origin = "flathub"; }
+      # WiVRn, SteamVR alternative
+      { appId = "io.github.wivrn.wivrn"; origin = "flathub"; }
     ];
   };
   # Flatpak requires this functionality
